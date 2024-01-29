535ce575d2300e5ab0b5f2ff084f8805f0b8da5b
kinire98 <iknitgarri@outlook.com>
2024-01-29 13:20:12 +0100

    first commit

---
+++ plugins.lua
@@ -0,0 +1,18 @@
+  .git
+  .github
+  lua
+    core
+       bootstrap.lua
+       default_config.lua
+       init.lua
+       mappings.lua
+       utils.lua
+    custom
+       chadrc.lua
+    plugins
+   .gitignore
+  󰈚 .ignore
+   .stylua.toml
+   init.lua
+   lazy-lock.json
+   LICENSE
